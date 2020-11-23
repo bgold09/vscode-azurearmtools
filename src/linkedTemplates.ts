76c7292e4440561c819b369d6b693bbb74f3378a
Stephen Weatherford <stephwe@microsoft.com>
2020-11-23 15:53:30 -0800

    work

--- src/linkedTemplates.ts
+++ src/linkedTemplates.ts
@@ -4,28 +4,34 @@
 
 import * as path from 'path';
 import { ProgressLocation, Uri, window, workspace } from "vscode";
-import { callWithTelemetryAndErrorHandling, IActionContext, parseError } from "vscode-azureextensionui";
+import { callWithTelemetryAndErrorHandling, IActionContext, parseError, TelemetryProperties } from "vscode-azureextensionui";
 import { ext } from "./extensionVariables";
 import { assert } from './fixed_assert';
+import { pathExists } from './util/pathExists';
 
+/**
+ * Inputs for RequestOpenLinkedFile request sent from language server
+ */
 export interface IRequestOpenLinkedFileArgs {
     sourceTemplateUri: string;
-    requestedLinkUri: string;
+    requestedLinkOriginalUri: string;
     requestedLinkResolvedUri: string;
-    //requestId: string;
-    //asdfrequestedLinkResolvedUriWithId: string;
+    pathType: PathType;
 }
 
+/**
+ * Response sent back to language server from RequestOpenLinkedFile request
+ */
 export interface IRequestOpenLinkedFileReturn {
-    errorMessage: string;
+    loadErrorMessage: string;
 }
 
-//asdf
-// enum PathType {
-//     templateLink = 0,
-//     templateRelativeLink = 1,
-//     parametersLink = 2,
-// }
+// Defined in the validation assembly used by the language server
+enum PathType {
+    templateLink = 0,
+    templateRelativeLink = 1,
+    parametersLink = 2,
+}
 
 interface ILinkedTemplate {
     id: string; // Guid
@@ -48,35 +54,33 @@ export interface INotifyTemplateGraphArgs {
  * @param sourceTemplateUri The full URI of the template which contains the link
  * @param requestedLinkPath The full URI of the resolved link being requested
  */
-export async function onRequestOpenLinkedFile({ sourceTemplateUri, requestedLinkResolvedUri }: IRequestOpenLinkedFileArgs): Promise<string | undefined> { //asdf returns error message
+export async function onRequestOpenLinkedFile({ sourceTemplateUri, requestedLinkResolvedUri, pathType }: IRequestOpenLinkedFileArgs): Promise<string | undefined> { //asdf returns error message
     //asdf do we try to keep this file around for a while???
 
     //asdf what return?
     return await callWithTelemetryAndErrorHandling('tryOpenLinkedFile', async (context: IActionContext) => { //asdf error handling
-        context.telemetry.properties.openResult = 'Failed';
-        context.telemetry.properties.openErrorType = '';
-
-        // e.g. 'https://fake/public-ip.json?link%3D0'
-        //asdf const contextId: string | undefined = (requestedLinkResolvedUriWithId.match(/link%3d([0-9]+$)/i) ?? [])[1]; //asdf
+        const properties = <TelemetryProperties & {
+            openResult: 'Loaded' | 'Error';
+            openErrorType: string;
+        }>context.telemetry.properties;
+        properties.openErrorType = '';
+        properties.openResult = 'Error';
 
         //asdf how properly handle paths like /proj/c#/file.txt?
-        const sourceTemplatePathAsUri: Uri = Uri.parse(sourceTemplateUri, true); //asdf? what if not file:// ?
-        const requestedLinkPathAsUri: Uri = Uri.parse(requestedLinkResolvedUri, true); //asdf? what if not file:// ?   // e.g. 'file:///Users/stephenweatherford/repos/vscode-azurearmtools/test/templates/linkedTemplates/parent-child//linkedTemplates/linkedTemplate.json?linked=%2FUsers%2Fstephenweatherford%2Frepos%2Fvscode-azurearmtools%2Ftest%2Ftemplates%2FlinkedTemplates%2Fparent-child%2FmainTemplate.json'
+        const sourceTemplatePathParsed: Uri = Uri.parse(sourceTemplateUri, true); //asdf? what if not file:// ?
+        const requestedLinkResolvedPathParsed: Uri = Uri.parse(requestedLinkResolvedUri, true); //asdf? what if not file:// ?
 
-        assert(path.isAbsolute(sourceTemplatePathAsUri.fsPath), "Internal error: sourceTemplateUri should be an absolute path");
-        assert(path.isAbsolute(requestedLinkPathAsUri.fsPath), "Internal error: requestedLinkUri should be an absolute path");
+        assert(path.isAbsolute(sourceTemplatePathParsed.fsPath), "Internal error: sourceTemplateUri should be an absolute path");
+        assert(path.isAbsolute(requestedLinkResolvedPathParsed.fsPath), "Internal error: requestedLinkUri should be an absolute path");
 
-        // const uri = Uri.parse(requestedLinkResolvedUriWithId); //asdf (= converted to %3D)
-        const uri = requestedLinkPathAsUri;
-        ext.outputChannel.appendLine(`Opening linked file "${uri}" in editor (linked from "${sourceTemplatePathAsUri.fsPath}")`);
-
-        //asdf what if get multiple requests immediately?  do we care?
-        const loadError = await tryOpenLinkedFile(uri);
+        // Strip the path of any query string, and use only the local file path
+        const localPath = requestedLinkResolvedPathParsed.fsPath;
+        const loadError = await tryOpenLinkedFile(localPath, pathType);
         if (loadError === undefined) {
-            context.telemetry.properties.openResult = 'Success';
+            properties.openResult = 'Loaded';
         } else {
             const parsedError = parseError(loadError);
-            context.telemetry.properties.openErrorType = parsedError.errorType;
+            properties.openErrorType = parsedError.errorType;
             return parsedError.message;
         }
     });
@@ -90,19 +94,19 @@ export async function onNotifyTemplateGraph(args: INotifyTemplateGraphArgs): Pro
     ext.outputChannel.appendLog(JSON.stringify(args, undefined, 4));
 }
 
-type PossibleError = { message?: string } | unknown;
+type PossibleError = Partial<Error>; //asdf move
 
 /**
  * Attempts to load the given file into a text document in VS Code so that
  * it will get sent to the language server.
  */
-async function tryOpenLinkedFile(uri: Uri): Promise<PossibleError | undefined> {
+async function tryOpenLinkedFile(localPath: string, pathType: PathType): Promise<Partial<Error> | undefined> {
     //asdf
     //await callWithTelemetryAndErrorHandling('tryLoadLinkedFile', async (actionContext: IActionContext) => { //asdf error handling
     return await window.withProgress<PossibleError | undefined>(//asdf?
         {
             location: ProgressLocation.Window,
-            title: `Loading linked file ${uri.fsPath}`
+            title: `Loading linked file ${localPath}`
         },
         async (): Promise<PossibleError | undefined> => {
             // Note: If the URI is already opened, returns the existing document
@@ -110,21 +114,32 @@ async function tryOpenLinkedFile(uri: Uri): Promise<PossibleError | undefined> {
             //uri = Uri.parse(uri.fsPath + "?a");
 
             try {
-                const doc = await workspace.openTextDocument(uri);
+                // Check first if the path exists, so we get a better error message if not
+                if (!(await pathExists(localPath))) {
+                    return <PossibleError>{
+                        message:
+                            pathType === PathType.parametersLink ?
+                                `Could not find linked parameter file "${localPath}"` :
+                                `Could not find linked template file "${localPath}"`,
+                        errorCode: "NotFound"
+                    };
+                }
+
+                // Load into a text document (this does not cause the document to be shown)
+                const doc = await workspace.openTextDocument(localPath);
                 // tslint:disable-next-line: no-console asdf
                 console.log(`... Opened: ${doc.uri}`);
-            } catch (err) {
-                ext.outputChannel.appendLine(`... Failed loading ${uri.fsPath}: ${parseError(err).message}`);
-                return err; //asdf what UI experience? put in error list?  asdf wrap error
+            } catch (err: unknown) {
+                ext.outputChannel.appendLine(`... Failed loading ${localPath}: ${parseError(err).message}`);
+                return <PossibleError>err; //asdf what UI experience? put in error list?  asdf wrap error
             }
 
-            ext.outputChannel.appendLine(`... Succeeded loading (or is already loaded) ${uri}`); //asdf
+            ext.outputChannel.appendLine(`... Succeeded loading (or is already loaded) ${localPath}`); //asdf
             //asdf await window.showTextDocument(doc);
 
             // No errors
             return undefined;
         }
-        //asdf What if it's JSON?  Will auto language switch kick in?
     );
 
 }
